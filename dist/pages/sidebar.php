85e1bb3a685428bb593595adfa8b1b0548fdff78
Moh. Zahri Valent Affandi <valent14ival@gmail.com>
2025-05-29 01:07:51 +0700

    menu admin

---
+++ dist/pages/sidebar.php
@@ -0,0 +1,72 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+        <!--begin::Sidebar Brand-->
+        <div class="sidebar-brand">
+          <!--begin::Brand Link-->
+          <a href="./index.html" class="brand-link">
+            <!--begin::Brand Image-->
+            <img
+              src="../../dist/assets/img/AdminLTELogo.png"
+              alt="AdminLTE Logo"
+              class="brand-image opacity-75 shadow"
+            />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">AdminLTE 4</span>
+            <!--end::Brand Text-->
+          </a>
+          <!--end::Brand Link-->
+        </div>
+        <!--end::Sidebar Brand-->
+        <!--begin::Sidebar Wrapper-->
+        <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              <li class="nav-item menu-open">
+                <a href="index.php" class="nav-link active">
+                  <i class="nav-icon bi bi-speedometer"></i>
+                  <p>
+                    Dashboard
+                    
+                  </p>
+                </a>
+      
+              </li>
+           
+           <!-- produk kita -->
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon bi bi-tree-fill"></i>
+                  <p>
+                    Produk 
+                    <i class="nav-arrow bi bi-chevron-right"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+                  <li class="nav-item">
+                    <a href="list.php" class="nav-link">
+                      <i class="nav-icon bi bi-circle"></i>
+                      <p>List produk</p>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                      <a href="pesanan.php" class="nav-link">
+                          <i class="nav-icon bi bi-circle"></i>
+                          <p>pesan produk</p>
+                      </a> 
+                  </li>
+                 
+                </ul>
+              </li>
+             
+            
+            <!--end::Sidebar Menu-->
+          </nav>
+        </div>
+        <!--end::Sidebar Wrapper-->
+      </aside>
